b8d5eeea061599b5be775aebe2d5a5c76c260978
Aren Leishman <aren.leishman@gmail.com>
2025-12-25 16:54:33 +1300

    Format HUD control and get it set up to work properly

--- Core/Src/HUDControl.c
+++ Core/Src/HUDControl.c
@@ -11,60 +11,70 @@ extern osMessageQueueId_t CellStatQueueHandle;
 
 static inline int16_t div10_round(int16_t x)
 {
-  /* rounds x/10 to nearest integer, handles negatives safely via int64_t */
-  return (int16_t)(((int32_t)x + (x >= 0 ? 5 : -5)) / 10);
+    /* rounds x/10 to nearest integer, handles negatives safely via int64_t */
+    return (int16_t)(((int32_t)x + (x >= 0 ? 5 : -5)) / 10);
 }
 
 static inline bool cell_alert(uint8_t cellVal)
 {
-  return (cellVal < 40 || cellVal > 165);
+    return (cellVal < 40 || cellVal > 165);
 }
 
-volatile bool alerting = false;
-void RGBBlinkControl()
+void PPO2Blink(CellValues_t *cellValues, bool *alerting)
 {
     const uint8_t centerValue = 100;
+    /* Dequeue the latest PPO2 information */
+    osStatus_t osStat = osMessageQueueGet(PPO2QueueHandle, &cellValues, NULL, 0);
+    if (osStat != osOK)
+    {
+        blinkNoData();
+    }
+    else if (cell_alert(cellValues->C1) || cell_alert(cellValues->C2) || cell_alert(cellValues->C3))
+    {
+        *alerting = true;
+        blinkAlarm();
+    }
+    else
+    {
+        *alerting = false;
+        osDelay(TIMEOUT_500MS_TICKS); /* Use an extra delay to "partition" the segments */
+    }
+
+    int16_t c1 = cellValues->C1 - centerValue;
+    int16_t c2 = cellValues->C2 - centerValue;
+    int16_t c3 = cellValues->C3 - centerValue;
+
+    c1 = div10_round(c1);
+    c2 = div10_round(c2);
+    c3 = div10_round(c3);
+
+    uint8_t failMask = ((cellValues->C1 == 0xFF ? 0 : 1) << 0) |
+                       ((cellValues->C2 == 0xFF ? 0 : 1) << 1) |
+                       ((cellValues->C3 == 0xFF ? 0 : 1) << 2);
+
+    uint8_t statusMask = 0b111;
+    osStat = osMessageQueueGet(CellStatQueueHandle, &statusMask, NULL, 0);
+    if (osStat != osOK)
+    {
+        statusMask = 0b111; // Default to all good if no status available
+    }
+
+    blinkCode((int8_t)c1, (int8_t)c2, (int8_t)c3, statusMask, failMask);
+}
+
+bool alerting = false;
+void RGBBlinkControl()
+{
+    /* We have LED control, turn the blue LEDS on while we wait for a signal */
+    for(uint8_t channel = 0; channel < 3; channel++)
+    {
+        setRGB(channel, 0, 0, 3); // Blue
+    }
     osDelay(TIMEOUT_2S_TICKS); /* Initial delay to for the DiveCAN system to start up and prime the queue */
     CellValues_t cellValues = {0};
     for (;;)
     {
-        /* Dequeue the latest PPO2 information */
-        osStatus_t osStat = osMessageQueueGet(PPO2QueueHandle, &cellValues, NULL, 0);
-        if (osStat != osOK)
-        {
-            blinkNoData();
-        }
-        else if (cell_alert(cellValues.C1) || cell_alert(cellValues.C2) || cell_alert(cellValues.C3))
-        {
-            alerting = true;
-            blinkAlarm();
-        }
-        else
-        {
-            alerting = false;
-            osDelay(TIMEOUT_500MS_TICKS); /* Use an extra delay to "partition" the segments */
-        }
-
-        int16_t c1 = cellValues.C1 - centerValue;
-        int16_t c2 = cellValues.C2 - centerValue;
-        int16_t c3 = cellValues.C3 - centerValue;
-
-        c1 = div10_round(c1);
-        c2 = div10_round(c2);
-        c3 = div10_round(c3);
-
-        uint8_t failMask = ((cellValues.C1 == 0xFF ? 0 : 1) << 0) |
-                           ((cellValues.C2 == 0xFF ? 0 : 1) << 1) |
-                           ((cellValues.C3 == 0xFF ? 0 : 1) << 2);
-
-        uint8_t statusMask = 0b111;
-        osStat = osMessageQueueGet(CellStatQueueHandle, &statusMask, NULL, 0);
-        if (osStat != osOK)
-        {
-            statusMask = 0b111; // Default to all good if no status available
-        }
-
-        blinkCode((int8_t)c1, (int8_t)c2, (int8_t)c3, statusMask, failMask);
+        PPO2Blink(&cellValues, &alerting);
     }
 }
 
